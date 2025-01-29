ab6783c12984fe155acc6a0dabf6bc0e19cd859e
ndrslmpk <andreas.lempik@gmail.com>
2025-01-29 10:47:58 +0100

    add orderbook with sorting interfaces

--- orderbook.go
+++ orderbook.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"time"
 )
 
@@ -20,6 +21,13 @@ type Order struct {
 
 }
 
+type Orders []*Order
+
+func (o Orders) Len() int           { return len(o) }
+func (o Orders) Swap(i, j int)      { o[i], o[j] = o[j], o[i] }
+func (o Orders) Less(i, j int) bool { return o[i].Timestamp < o[j].Timestamp }
+
+// Bid if true describes the price for a seller to sell, if false describes the price for a buyer to buy
 func NewOrder(bid bool, size float64) *Order {
 	return &Order{
 		Size:      size,
@@ -34,9 +42,23 @@ func (o *Order) String() string {
 
 type Limit struct {
 	Price       float64
-	Orders      []*Order
+	Orders      Orders
 	TotalVolume float64
 }
+
+type Limits []*Limit
+
+type ByBestAsk struct{ Limits }
+
+func (a ByBestAsk) Len() int           { return len(a.Limits) }
+func (a ByBestAsk) Swap(i, j int)      { a.Limits[i], a.Limits[j] = a.Limits[j], a.Limits[i] }
+func (a ByBestAsk) Less(i, j int) bool { return a.Limits[i].Price < a.Limits[j].Price }
+
+type ByBestBid struct{ Limits }
+
+func (b ByBestBid) Len() int           { return len(b.Limits) }
+func (b ByBestBid) Swap(i, j int)      { b.Limits[i], b.Limits[j] = b.Limits[j], b.Limits[i] }
+func (b ByBestBid) Less(i, j int) bool { return b.Limits[i].Price > b.Limits[j].Price }
 
 func NewLimit(price float64) *Limit {
 	return &Limit{
@@ -63,6 +85,7 @@ func (l *Limit) DeleteOrder(o *Order) {
 	l.TotalVolume -= o.Size
 
 	// TODO: resort the orders
+	sort.Sort(l.Orders)
 }
 
 type Orderbook struct {
@@ -83,9 +106,39 @@ func NewOrderbook() *Orderbook {
 }
 
 func (ob *Orderbook) PlaceOrder(price float64, o *Order) []Match {
+	// 1. Try to match the orders
+	// matching logic
 
+	// 2. add the rest of the order to the books
+	if o.Size >= 0 {
+		ob.add(price, o)
+		// ob.sort()
+	}
+
+	return []Match{}
 }
 
 func (ob *Orderbook) add(price float64, o *Order) {
+	var limit *Limit
 
+	if o.Bid {
+		limit = ob.BidLimits[price]
+	} else {
+		limit = ob.AskLimits[price]
+	}
+
+	// If no limit with the given price exists, then create a new limit
+	if limit == nil {
+		limit = NewLimit(price)
+		limit.AddOrder(o)
+
+		if o.Bid {
+			ob.Bids = append(ob.Bids, limit)
+			ob.BidLimits[price] = limit
+		} else {
+			ob.Asks = append(ob.Asks, limit)
+			ob.AskLimits[price] = limit
+		}
+
+	}
 }
--- orderbook_test.go
+++ orderbook_test.go
@@ -22,4 +22,20 @@ func TestLimit(t *testing.T) {
 
 func TestOrderbook(t *testing.T) {
 	ob := NewOrderbook()
+
+	buyOrderA := NewOrder(true, 10)
+	buyOrderB := NewOrder(true, 2000)
+	buyOrderC := NewOrder(true, 100)
+
+	ob.PlaceOrder(100_000, buyOrderA)
+	ob.PlaceOrder(110_000, buyOrderB)
+
+	ob.PlaceOrder(100_000, buyOrderC)
+
+	fmt.Printf("%+v", ob.Bids[0])
+	// fmt.Printf("%+v", ob.Bids[2])
+
+	// for i := 0; i < len(ob.Bids); i++ {
+	// 	fmt.Printf("%+v\n", ob.Bids[i])
+	// }
 }
